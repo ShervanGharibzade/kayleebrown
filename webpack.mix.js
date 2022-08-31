ddf0c63eeff6718d166c1a7c0428db863375bf47
shervan <shervangh@gmail.com>
2022-09-01 00:11:48 +0430

    deploy

--- webpack.mix.js
+++
@@ -1,7 +0,0 @@
-let mix = require('laravel-mix');
-
-mix
-    .sass('resources/scss/default.scss', 'assets/css/default.css')
-        .setPublicPath('public')
-    .js('resources/js/app.js', 'assets/js/app.js')
-        .setPublicPath('public');
